88defe180de98de5cad7db375ffebbc5f05118fa
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2024-01-12 09:35:35 -0500

    Sample form for posts

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsBase
+module DemoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
